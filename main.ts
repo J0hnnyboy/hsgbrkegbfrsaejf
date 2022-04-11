f19205c77610cf96e72cbb9e5d3a40289b1b030e
J0hnnyboy <101340085+J0hnnyboy@users.noreply.github.com>
2022-04-11 09:24:38 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let Number_1 = 2
+let Number_2 = 3
+let Number_3 = 4
+if (Number_1 > 1 || Number_2 > 2) {
+    basic.showIcon(IconNames.Heart)
+} else {
+    basic.showIcon(IconNames.Happy)
+}
+basic.forever(function () {
+	
+})
